546ceba50273391efbc7eafbbfc87c88535ff22e
Roza Rostam Nejad <roza.rostam.nejad@ut.ee>
2025-07-19 18:19:38 +0300

    updated

--- cellranger.sh
+++ cellranger.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J cellranger_BM1  #job name
 #SBATCH --partition=amd        #Partition/queue to run job #GPU
-#SBATCH -t 3:00:00               #max time 3 hours
+#SBATCH -t 6:00:00               #max time 3 hours
 #SBATCH --cpus-per-task=32        #nr os cpu requested
-#SBATCH --mem=156GB 
+#SBATCH --mem=256GB 
 
 CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/config.csv
 
@@ -13,7 +13,7 @@ cellranger multi \
   --id=GEX_BM1_multi \
   --csv=$CONFIG_CSV \
   --localcores=32 \
-  --localmem=156
+  --localmem=256
 
 
 
