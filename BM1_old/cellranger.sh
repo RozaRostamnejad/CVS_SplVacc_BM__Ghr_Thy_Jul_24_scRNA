e5cbdeb58dd4be77bd33309db5296b8ff867e7c6
Roza Rostam Nejad <roza.rostam.nejad@ut.ee>
2025-11-08 19:11:13 +0200

    X_old for exmaple BM1_old are the first runs without [samples] included in their config.csv, the name changed BM1 -> BM1_old and new directory was created with the same name i.e.BM1

---
+++ BM1_old/cellranger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J CR_BM1  #job name
+#SBATCH --partition=amd        #Partition/queue to run job #GPU
+#SBATCH -t 6:00:00               #max time 3 hours
+#SBATCH --cpus-per-task=32        #nr os cpu requested
+#SBATCH --mem=256GB 
+
+CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/BM1/config.csv
+
+
+cellranger multi \
+  --id=GEX_BM1_multi \
+  --csv=$CONFIG_CSV \
+  --localcores=32 \
+  --localmem=256
+
+
+
+
+
---
+++ BM2_new/cellranger_BM2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J CR_BM2
+#SBATCH --partition=amd
+#SBATCH -t 10:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256GB
+
+CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/BM2/config.csv
+
+
+cellranger multi \
+  --id=GEX_BM2_multi \
+  --csv=$CONFIG_CSV \
+  --localcores=32 \
+  --localmem=256
