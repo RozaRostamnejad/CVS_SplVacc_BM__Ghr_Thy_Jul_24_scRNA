3564a1ee633c2ea2d21ca8676c3da0eea3e3123e
Roza Rostam Nejad <roza.rostam.nejad@ut.ee>
2025-11-11 13:03:13 +0200

    updated BM2 cellranger.sh

--- BM2/cellranger_BM2.sh
+++ BM2/cellranger_BM2.sh
@@ -5,11 +5,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=256GB
 
-CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/BM2/config.csv
-
-
 cellranger multi \
-  --id=GEX_BM2_multi \
-  --csv=$CONFIG_CSV \
+  --id=GEX_BM2_new_multi \
+  --csv=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/BM2/config.csv \
   --localcores=32 \
   --localmem=256
