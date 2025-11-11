450e88e0a1d073b78265fad4e734a4d440d0329f
Roza Rostam Nejad <roza.rostam.nejad@ut.ee>
2025-11-11 15:21:25 +0200

    added inputs

--- GT2/cellranger.sh
+++ GT2/cellranger.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#SBATCH -J CR_GT2  #job name
-#SBATCH --partition=amd        #Partition/queue to run job #GPU
-#SBATCH -t 6:00:00               #max time 3 hours
-#SBATCH --cpus-per-task=32        #nr os cpu requested
-#SBATCH --mem=256GB 
+#SBATCH -J CR_GT2_new
+#SBATCH --partition=amd
+#SBATCH -t 10:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256GB
 
 CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/GT2/config.csv
 
 
 cellranger multi \
-  --id=GEX_GT2_multi \
+  --id=GEX_GT2_new_multi \
   --csv=$CONFIG_CSV \
   --localcores=32 \
   --localmem=256
-
-
-
-
-
--- GT3/cellranger.sh
+++ GT3/cellranger.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#SBATCH -J CR_GT3  #job name
-#SBATCH --partition=amd        #Partition/queue to run job #GPU
-#SBATCH -t 6:00:00               #max time 3 hours
-#SBATCH --cpus-per-task=32        #nr os cpu requested
-#SBATCH --mem=256GB 
+#SBATCH -J CR_GT3_new
+#SBATCH --partition=amd
+#SBATCH -t 10:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256GB
 
 CONFIG_CSV=/gpfs/helios/home/rostamne/CVS_SplVacc_BM__Ghr_Thy_Jul_24_scRNA/code/GT3/config.csv
 
 
 cellranger multi \
-  --id=GEX_GT3_multi \
+  --id=GEX_GT3_new_multi \
   --csv=$CONFIG_CSV \
   --localcores=32 \
   --localmem=256
-
-
-
-
 
